f030d8bbf1a9251e14eb398f11ca2a30d0266e79
Rogelio Zamudio <yeyozaz@gmail.com>
2017-06-09 23:38:07 -0500

    new helpers, username migration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def user_id_to_email(user_id)
     return User.find(params[:id]).email
   end
+
+  def user_id_to_username(user_id)
+    return User.find(params[:id]).username
+  end
 end
