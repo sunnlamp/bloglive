6399bea9ab2847d0c7e7cab5566500e1a5a41086
Rogelio Zamudio <yeyozaz@gmail.com>
2017-06-09 22:39:29 -0500

    starting new branch

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,9 +6,8 @@ before_action :find_commentable
   end
 
   def create
-
+    @comment.user_id = current_user.id
     @comment = @commentable.comments.new(comment_params)
-
 
     respond_to do |format|
       if @comment.save
@@ -27,7 +26,7 @@ before_action :find_commentable
       params.require(:comment).permit(:body)
     end
     def set_comment
-      @comment = Post.find(params[:id])
+      @comment = Post.find(:post_id, :user_id, params[:id])
     end
     def find_commentable
       @commentable = Comment.find_by_id(params[:comment_id]) if params[:comment_id]
