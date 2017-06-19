667c431303324aae7e79e4ff2242e86923e88e6b
Rogelio Zamudio <yeyozaz@gmail.com>
2017-06-19 11:05:23 -0500

    users index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-
   root to: 'home#index'
+
+  match '/users/',   to: 'users#index',   via: 'get'
+  match '/users/:id',     to: 'users#show',       via: 'get'
+
   devise_for :users do
+    devise_for :users, :path_prefix => 'd'
+    resources :users, :only =>[:show]
     member do
       get :following, :followers
     end
